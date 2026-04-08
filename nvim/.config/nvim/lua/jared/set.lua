1221092e18ca7293da2e022b33b01daff367abbf
Jared Swift <jared.swift@kcl.ac.uk>
2026-04-08 01:48:05 +0100

    Don't use a plugin for colour scheme.

--- nvim/.config/nvim/lua/jared/set.lua
+++ nvim/.config/nvim/lua/jared/set.lua
@@ -1,3 +1,29 @@
+-- Colour scheme
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("habamax")
+local function set_transparent() -- set UI component to transparent
+	local groups = {
+		"Normal",
+		"NormalNC",
+		"EndOfBuffer",
+		"NormalFloat",
+		"FloatBorder",
+		"SignColumn",
+		"StatusLine",
+		"StatusLineNC",
+		"TabLine",
+		"TabLineFill",
+		"TabLineSel",
+		"ColorColumn",
+	}
+	for _, g in ipairs(groups) do
+		vim.api.nvim_set_hl(0, g, { bg = "none" })
+	end
+	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#767676" })
+end
+
+set_transparent()
+
 vim.opt.number = true -- line number
 vim.opt.relativenumber = true -- relative line numbers
 vim.opt.cursorline = true -- highlight current line
